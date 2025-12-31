56de552930a24619d9f072169e1bbe89597e918b
Hikmet AKGUN <37314398-hikmetakgun94@users.noreply.replit.com>
2025-12-31 02:45:40 +0000

    return-old-static-settings

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ else:
 
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
     AWS_ACCESS_KEY_ID = DJANGO_ACCESS_KEY_ID
 
